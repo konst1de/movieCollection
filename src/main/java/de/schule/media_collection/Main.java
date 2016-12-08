3381bd067d64390a94b73953dd94022d9e17e438
Konstantin Vogel <Konstantin.vogel56@gmail.com>
2016-12-08 13:14:25 +0100

    starting parameters to determine what interface and what storing method have to be used.

--- src/main/java/de/schule/media_collection/Main.java
+++ src/main/java/de/schule/media_collection/Main.java
@@ -8,14 +8,21 @@ import de.schule.media_collection.view.View;
 public class Main{
 	
 	public static void main(String[] args){
-		try {
-			Tui tui = new Tui();
-			tui.menu();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		boolean useSQL = args.length > 0 && "--use-sql".equals(args[0]);
+		boolean useGUI = args.length > 1 && "--use-gui".equals(args[1]);
+		
+		if(useGUI){
+			View.launch(View.class, args);
+		}else{
+			try {
+				Tui tui = new Tui();
+				tui.menu();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-//		View.launch(View.class, args);
+		
 		
 	}
 }
